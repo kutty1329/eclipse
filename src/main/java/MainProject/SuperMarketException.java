990af1a5e1b28dda50ac60f31ca70a722956fbba
kutty1329 <hashtageditinginfotech@gmail.com>
2022-11-18 14:00:09 +0530

    Exception

---
+++ src/main/java/MainProject/SuperMarketException.java
@@ -0,0 +1,10 @@
+package MainProject;
+
+public class SuperMarketException extends Exception
+{
+	
+ public SuperMarketException()
+ {
+	 super("#SuperMarket values are not matching our field");
+ }
+}

7aa386b806e41696286feb6f02a7ed28f91b4523
kutty1329 <hashtageditinginfotech@gmail.com>
2022-12-04 07:35:34 +0530

    Main

--- src/main/java/MainProject/AppMarket.java
+++ src/main/java/MainProject/AppMarket.java
@@ -6,8 +6,19 @@ public class AppMarket {
 		// TODO Auto-generated method stub
 		
 		Market user=new Market();
-		Thread th1=new Thread(user,"ManojKumar");
+		Thread th=new Thread(user,"ManojKumar");
+		Thread th1=new Thread(user,"Thivin");
+		Thread th2=new Thread(user,"Thilak");
+		Thread th3=new Thread(user,"Vishnu");
+		Thread th4=new Thread(user,"Ganesh");
+		Thread th5=new Thread(user,"Pradeep");
+		
+		th.start();
 		th1.start();
+		th2.start();
+		th3.start();
+		th4.start();
+		th5.start();
 
 	}
 
